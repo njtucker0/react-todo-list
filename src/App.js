d1d3d049dffcfd83fae10ad56c109a81bcbabe89
njtucker0 <120738291+njtucker0@users.noreply.github.com>
2023-08-30 07:09:22 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React from "react";
-import PageTitle from "./components/PageTitle";
-import AppHeader from "./components/AppHeader";
-import styles from "./styles/modules/app.module.scss";
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
+import AppContent from './components/AppContent';
+import AppHeader from './components/AppHeader';
+import PageTitle from './components/PageTitle';
+import styles from './styles/modules/app.module.scss';
 
 function App() {
   return (
-    <div className="container">
-      <PageTitle>TODO LIST</PageTitle>
-      <div className={styles.app__wrapper}>
-        <AppHeader />
+    <>
+      <div className="container">
+        <PageTitle>TODO List</PageTitle>
+        <div className={styles.app__wrapper}>
+          <AppHeader />
+          <AppContent />
+        </div>
       </div>
-    </div>
+      <Toaster
+        position="bottom-right"
+        toastOptions={{
+          style: {
+            fontSize: '1.4rem',
+          },
+        }}
+      />
+    </>
   );
 }
 
-export default App;
+export default App;;
